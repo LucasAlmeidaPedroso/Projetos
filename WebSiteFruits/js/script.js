58d4d8eb78f86d6bd873ba9b872582b31ed95c56
Lucas Pedroso <85121714+LucasPedroso42@users.noreply.github.com>
2023-03-15 17:31:58 -0300

    Add files via upload

---
+++ WebSiteFruits/js/script.js
@@ -0,0 +1,8 @@
+function openwindow(){
+    document.getElementById("saibamais").style.display = "block";
+    document.getElementById("saibamais").style.width = "100vh"
+    document.getElementById("saibamais").style.height = "60vh"
+}
+function closewindow(){
+    document.getElementById("saibamais").style.display = "none";
+}
