5b6729c20a8c378577ed6ee9d3254e51478984ba
Lucas Pedroso <lucasalmeidapedroso@gmail.com>
2023-04-13 17:56:46 -0300

    Alterações hospital

--- WebSiteHospitalResponsive/js/script.js
+++ WebSiteHospitalResponsive/js/script.js
@@ -1,10 +1,9 @@
-let menu = document.getElementById('menu-btn');
-let navbar = document.querySelector('.navbar');
+menu = document.querySelector('#menu-btn');
+navbar = document.querySelector('.navbar');
 
-menu.onclick = () =>{
+menu.onclick = (a) =>{
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
-    alert('deu certo');
 }
 window.onscroll = () =>{
     menu.classList.remove('fa-times');
