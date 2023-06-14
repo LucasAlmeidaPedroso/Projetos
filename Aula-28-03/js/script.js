ed67bcf7d5785768fc008c25149c475b5e0b2e3a
Lucas Pedroso <lucasalmeidapedroso@gmail.com>
2023-06-14 00:42:53 -0300

    Projetos inseridos

--- Aula-28-03/js/script.js
+++
@@ -1,9 +0,0 @@
-function mostrarData(){
-    document.getElementById('view').innerHTML = Date();
-};
-function girar(){
-
-};
-function mudartexto(){
-    document.getElementById('textoasermudado').innerHTML = "O JOGO É NÃO PENSAR NO JOGO"
-}
---
+++ JavaScript/Conversor-Binario/js/index.js
@@ -0,0 +1,4 @@
+function calculate() {
+  var text = document.getElementById("decimal").value;
+  document.getElementById("Resultado").innerText = parseInt(text).toString(2)
+}
